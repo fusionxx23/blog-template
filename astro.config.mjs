1781633d4ee111b12b714639bdc6f48bac14599e
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-12-06 17:46:53 -0800

    feat: Adding integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import auth from "auth-astro";
+
+import sitemap from "@astrojs/sitemap";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://travistarpley.com",
+  output: "server",
+  integrations: [tailwind(), auth(), sitemap(), svelte()],
+});
